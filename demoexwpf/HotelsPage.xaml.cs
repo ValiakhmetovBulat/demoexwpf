4a6c15402e2c60239bb72de84a600c25834f9363
ValiakhmetovBulat <valiaxmetovb134@gmail.com>
2022-05-31 23:44:10 +0300

    Добавьте файлы проекта.

---
+++ demoexwpf/HotelsPage.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace demoexwpf
+{
+    /// <summary>
+    /// Логика взаимодействия для HotelsPage1.xaml
+    /// </summary>
+    public partial class HotelsPage1 : Page
+    {
+        public HotelsPage1()
+        {
+            InitializeComponent();
+            DGridHotels.ItemsSource = ToursEntities.GetContext().Hotel.ToList();
+        }
+
+        private void BtnEdit_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.Navigate(new AddEditPage((sender as Button).DataContext as Hotel));
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void Page_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            if (Visibility == Visibility.Visible)
+            {
+                ToursEntities.GetContext().ChangeTracker.Entries().ToList().ForEach(p => p.Reload());
+                DGridHotels.ItemsSource = ToursEntities.GetContext().Hotel.ToList();
+            }
+        }
+
+        private void DGridHotels_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.Navigate(new AddEditPage(null));
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            var hotelsForRemoving = DGridHotels.SelectedItems.Cast<Hotel>().ToList();
+
+            if (MessageBox.Show($"Вы точно хотите удалить следующие {hotelsForRemoving.Count()} элементов ?", "Внимание", MessageBoxButton.YesNo
+                , MessageBoxImage.Question) == MessageBoxResult.Yes)
+            {
+                try
+                {
+                    ToursEntities.GetContext().Hotel.RemoveRange(hotelsForRemoving);
+                    ToursEntities.GetContext().SaveChanges();
+                    MessageBox.Show("Данные удалены."); 
+
+                    DGridHotels.ItemsSource = ToursEntities.GetContext().Hotel.ToList();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message.ToString());
+                }
+            }
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.Navigate(new ToursPage());
+        }
+    }
+}
---
+++ demoexwpf/ToursPage.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace demoexwpf
+{
+    /// <summary>
+    /// Логика взаимодействия для ToursPage.xaml
+    /// </summary>
+    public partial class ToursPage : Page
+    {
+        public ToursPage()
+        {
+            InitializeComponent();
+
+            var allTypes = ToursEntities.GetContext().Type.ToList();
+            allTypes.Insert(0, new Type
+            {
+                Name = "Все типы"
+            }
+                );
+            ComboType.ItemsSource = allTypes;
+
+            CheckIsActual.IsChecked = true;
+            ComboType.SelectedIndex = 0;
+
+            UpdateTours();
+            //ChangeColor(LVTors.FindName("ISActual") as TextBlock);
+        }
+
+        private void UpdateTours()
+        {            
+            var currentTours = ToursEntities.GetContext().Tour.ToList();
+
+            if (ComboType.SelectedIndex > 0)
+                currentTours = currentTours.Where(p => p.Type.Contains(ComboType.SelectedItem as Type)).ToList();
+
+            currentTours = currentTours.Where(p => p.Name.ToLower().Contains(TextBoxSearch.Text.ToLower())).ToList();
+
+            if (CheckIsActual.IsChecked.Value)
+                currentTours = currentTours.Where(p => p.IsActual).ToList();
+
+            LVTors.ItemsSource = currentTours.OrderBy(p => p.TicketCount).ToList();            
+        }
+
+        private void TextBoxSearch_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            UpdateTours();
+        }
+
+        private void ComboType_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            UpdateTours();
+        }
+
+        private void CheckIsActual_Checked(object sender, RoutedEventArgs e)
+        {
+            UpdateTours();
+        }
+
+        private void CheckIsActual_Unchecked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ChangeColor(TextBlock text)
+        {
+            if (text.Text == "Актуален")
+                text.Foreground = Brushes.Green;
+            else
+                text.Foreground = Brushes.Red;
+        }
+    }
+}
